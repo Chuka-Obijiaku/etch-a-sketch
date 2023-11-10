ef88b54a42eb7673d09bdd051430fc6226b35c78
chuka <22582292+Chuka-Obijiaku@users.noreply.github.com>
2023-11-10 04:22:17 -0600

    Add functionality to the amount of cells on boars
    buttons call function to change amount of cells on board

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 // generate the grid of divs 16 x 16
 
-function generateGrid (gridDimension) {
-    let gridContainer = document.getElementById("grid-container");
-    for (let i = 0; i < gridDimension; i++) {
-        let row = document.createElement("div");
-        row.id = "row";
-        for (let j = 0; j < gridDimension; j++) {
-            let cell = document.createElement("div");
-            cell.id = "grid-square";
-            row.appendChild(cell);
-        }
-        gridContainer.appendChild(row);
+let drawColor = "#141414";
+const white = "#FFFFFF";
 
+
+function generateBoard (size) {
+    
+    
+    let board = document.getElementById("board");
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove())
+    board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size**2; i++) {
+        let cell = document.createElement("div");
+        cell.id = 'cell';
+        cell.style.backgroundColor = white;
+        board.insertAdjacentElement("beforeend", cell)
+
+       
     }
+    return
 }
 
-generateGrid(16);
+
+ 
+generateBoard(16);
+
+function changeSize(input) {
+    generateBoard(input);
+}
 // be carefull of margins and padding
 // setup a hover effect so grid divs change color 
 // add a button to the page that asks the number of square to generate 
