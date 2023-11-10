eb3f35edccc576349019a4bf946c7d721368f9f8
chuka <22582292+Chuka-Obijiaku@users.noreply.github.com>
2023-11-10 11:11:38 -0600

    Add  draw and erase functionality
    use draw and erase button to toggle

--- script.js
+++ script.js
@@ -9,11 +9,15 @@ function generateBoard (size) {
     
     let board = document.getElementById("board");
     let squares = board.querySelectorAll('div');
-    squares.forEach((div) => div.remove())
+    squares.forEach((div) => div.remove());
     board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     for (let i = 0; i < size**2; i++) {
         let cell = document.createElement("div");
+        cell.addEventListener('mouseover', ()=> {
+            
+            cell.style.backgroundColor = drawColor;
+        });
         cell.id = 'cell';
         cell.style.backgroundColor = white;
         board.insertAdjacentElement("beforeend", cell)
@@ -29,6 +33,10 @@ generateBoard(16);
 
 function changeSize(input) {
     generateBoard(input);
+}
+
+function changeColor(color) {
+    drawColor = color;
 }
 // be carefull of margins and padding
 // setup a hover effect so grid divs change color 
